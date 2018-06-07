891622387289fdd42fc946591b10d4c680e7623e
Bcepnez <senju_mr@hotmail.com>
2018-06-07 14:54:33 +0700

    modified show data area

--- app/src/main/java/com/bcepnez/mrz/MainActivity.java
+++ app/src/main/java/com/bcepnez/mrz/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.SparseArray;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     SurfaceView cameraview;
     TextView text;
+    TextView text2;
     CameraSource cameraSource;
     final int RequestCameraPermissionID = 1001;
 
@@ -52,19 +54,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        Button b1 = (Button)findViewById(R.id.button2);
+//        b1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent i = new Intent(MainActivity.this,MyCameraActivity.class);
+//                startActivity(i);
+//            }
+//        });
+
         cameraview = (SurfaceView) findViewById(R.id.surface_view);
         text = (TextView) findViewById(R.id.text_view);
+        text2 = (TextView) findViewById(R.id.text_view1);
 
         TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+
         if (!textRecognizer.isOperational()) {
-            Log.w("MainActivity", "Detectdependencier are not available");
+            Log.w("MainActivity", "Detect dependencier are not available");
         } else {
             cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
                     .setFacing(CameraSource.CAMERA_FACING_BACK)
-                    .setRequestedPreviewSize(1280, 1024)
-                    .setRequestedFps(2.0f)
+                    .setRequestedPreviewSize(1280, 720)
+                    .setRequestedFps(1.0f)
                     .setAutoFocusEnabled(true)
                     .build();
+
             cameraview.getHolder().addCallback(new SurfaceHolder.Callback() {
                 @Override
                 public void surfaceCreated(SurfaceHolder surfaceHolder) {
@@ -92,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
+
             textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
                 @Override
                 public void release() {
@@ -102,17 +117,43 @@ public class MainActivity extends AppCompatActivity {
                 public void receiveDetections(Detector.Detections<TextBlock> detections) {
                     final SparseArray<TextBlock> items = detections.getDetectedItems();
                     if (items.size()!=0){
+
                         text.post(new Runnable() {
+                            int index=0;
+                            String[] arr = new String[45];
+
                             @Override
                             public void run() {
+
                                 StringBuilder stringBuilder = new StringBuilder();
+                                StringBuilder stringBuilder2 = new StringBuilder();
                                 for (int i = 0;i<items.size();++i){
                                     TextBlock item = items.valueAt(i);
                                     stringBuilder.append(item.getValue());
+                                    stringBuilder2.append(items.size());
+                                    stringBuilder2.append(" ");
                                     stringBuilder.append("\n");
+
+                                    if(item.getValue().startsWith("P")&&
+                                            item.getValue().endsWith("<")&&
+                                            item.getValue().trim().length()>30) {
+                                        arr[index] = item.getValue();
+                                        index++;
+                                        for (int j =0;j<index;j++){
+                                            stringBuilder2.append("Found : ");
+                                            stringBuilder2.append(arr[j]);
+//                                            stringBuilder2.append(item.getValue());
+                                            stringBuilder2.append(" index : ");
+                                            stringBuilder2.append(arr[j].length());
+                                            stringBuilder2.append("\n");
+                                        }
+
+                                    }
                                 }
                                 text.setText(stringBuilder.toString());
+                                text2.setText(stringBuilder2.toString());
                             }
+
                         });
                     }
                 }
